229d304ae17a13a7c7c89e51f125e90cdd21c067
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-02 16:09:35 -0400

    setup repo

---
+++ my_test.py
@@ -0,0 +1,7 @@
+# testing-123/my_test.py
+
+from my_script import enlarge # load the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
+
+def test_enlarge(): # name this function anything, but hopefully something related to the name of the function it is testing
+    result = enlarge(3)
+    assert result == 300
