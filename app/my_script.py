41affcbb3499956db1a13986c4ffdb58bb34aa7e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-02 19:57:10 -0400

    Challenge 3

--- app/my_script.py
+++ app/my_script.py
@@ -2,3 +2,9 @@
 
 def enlarge(i):
     return i * 100
+
+if __name__ == "__main__": # "if this script is run from the command-line, then ..."
+    original_number = int(input("Please select a number to be enlarged (e.g. 400): "))
+    print("You chose: ", original_number)
+    bigger_number = enlarge(original_number)
+    print("Enlarged number is: ", bigger_number)
