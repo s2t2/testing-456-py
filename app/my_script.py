1731aefa3921cc32792f7e2214df4aa7c71933a6
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-02 21:15:41 -0400

    A breaking change

--- app/my_script.py
+++ app/my_script.py
@@ -1,7 +1,7 @@
 # testing-123/my_script.py
 
 def enlarge(i):
-    return i * 100
+    return i * 1
 
 if __name__ == "__main__": # "if this script is run from the command-line, then ..."
     original_number = int(input("Please select a number to be enlarged (e.g. 400): "))
